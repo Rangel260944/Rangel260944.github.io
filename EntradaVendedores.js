6f214d445e0d4bf1c11457f9fc2ad1bee8dea788
Rangel2609 <69400987+Rangel2609@users.noreply.github.com>
2021-12-12 12:51:19 -0600

    .

---
+++ EntradaVendedores.js
@@ -0,0 +1,12 @@
+{
+    "vendedores":
+    [
+        {
+            "id":1,
+            "nombre":"juan",
+            "edad": 25,
+            "correo": "juan@gmail.com",
+
+        }
+    ]
+}
--- Funciones.js
+++ Funciones.js
@@ -3,7 +3,11 @@
 
 function login(){
 
-    alert("Bienvenido usuario Administrador");
+    var mydata = JSON.parse(data);
+    alert(mydata[0].name);
+    alert(mydata[0].age);
+    alert(mydata[1].name);
+    alert(mydata[1].age);
 
 
 }
