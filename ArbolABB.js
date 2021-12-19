7b22c497cf04c57926b19d209bb690cff6ab4bea
Rangel2609 <69400987+Rangel2609@users.noreply.github.com>
2021-12-19 00:47:57 -0600

    Commit 19/12/2021

---
+++ ArbolABB.js
@@ -0,0 +1,123 @@
+class nodo{
+    constructor(dato, nombre){
+        this.dato = dato
+        this.nombre = nombre
+        this.izq = null
+        this.der = null
+    }
+}
+
+class abb{
+    constructor(){
+        this.raiz = null
+    }
+
+    insertar(valor, nombre){
+        let nuevo = new nodo(valor, nombre)
+
+        if(this.raiz == null){
+            this.raiz= nuevo
+        }else{
+            this.raiz = this.insertar_nodo(this.raiz,nuevo)
+        }
+    }
+
+    insertar_nodo(raiz_actual,nuevo){
+        if(raiz_actual != null){
+            //recorrer hijos
+            if(raiz_actual.dato > nuevo.dato){
+                raiz_actual.izq = this.insertar_nodo(raiz_actual.izq,nuevo)
+
+            }else if(raiz_actual.dato < nuevo.dato){
+                raiz_actual.der = this.insertar_nodo(raiz_actual.der,nuevo)
+            }else{
+                console.log("NO SE PUEDE INSERTAR EL DATO PORQUE YA EXISTE")
+            }
+
+            return raiz_actual
+        }else{
+            raiz_actual = nuevo
+            return raiz_actual
+        }
+    }
+
+    preorden(raiz_actual){
+        if(raiz_actual != null){
+            console.log(raiz_actual.dato)
+            this.preorden(raiz_actual.izq)
+            this.preorden(raiz_actual.der)
+        }
+    }
+
+    inOrden(raiz_actual){
+        if(raiz_actual != null){
+            this.inOrden(raiz_actual.izq)
+            console.log(raiz_actual.dato)
+            this.inOrden(raiz_actual.der)
+        }
+    }
+
+    postOrden(raiz_actual){
+        if(raiz_actual != null){
+            this.postOrden(raiz_actual.izq)
+            this.postOrden(raiz_actual.der)
+            console.log(raiz_actual.dato)
+        }
+    }
+
+    generarDot(){
+        let cadena="digraph arbol {\n"
+        cadena+= this.generar_nodos(this.raiz)
+        cadena+="\n"
+        cadena+=this.enlazar(this.raiz)
+        cadena+="\n}"
+
+        console.log(cadena)
+    }
+
+    generar_nodos(raiz_actual){ //metodo preorden
+        let nodos =""
+        if(raiz_actual != null){
+            nodos+= "n"+raiz_actual.dato+"[label=\"" + "Id: "+raiz_actual.dato+"\nNombre: "+ raiz_actual.nombre+ "\"]\n"
+            nodos+=this.generar_nodos(raiz_actual.izq)
+            nodos+=this.generar_nodos(raiz_actual.der)
+        }
+        return nodos
+    }
+
+    enlazar(raiz_actual){
+        let cadena=""
+        if(raiz_actual != null){
+            cadena += this.enlazar(raiz_actual.izq)
+            cadena += this.enlazar(raiz_actual.der)
+            //validaciones
+            if(raiz_actual.izq != null){
+                cadena+="n"+raiz_actual.dato + "-> n"+raiz_actual.izq.dato+"\n"
+            }
+            if(raiz_actual.der != null){
+                cadena+="n"+raiz_actual.dato + "-> n"+raiz_actual.der.dato+"\n"
+            }
+
+            
+        }
+        return cadena
+    }
+}
+
+arbol = new abb()
+arbol.insertar(30, 'Carlos')
+arbol.insertar(40, 'Katherine')
+arbol.insertar(20, 'Luis')
+arbol.insertar(10, 'Emily')
+arbol.insertar(5, 'Roberto')
+arbol.insertar(70, 'Daniel')
+arbol.insertar(7, 'Naomi')
+arbol.insertar(100, 'Cristian')
+console.log("***RECORRIDO PREORDEN****")
+arbol.preorden(arbol.raiz)
+console.log("***RECORRIDO InOrden****")
+arbol.inOrden(arbol.raiz)
+//console.log("***RECORRIDO PostOrden****");
+//arbol.postOrden(arbol.raiz);
+
+arbol.generarDot()
---
+++ Lista_Meses.js
@@ -0,0 +1,70 @@
+class Nodo{
+    constructor(mes){
+        this.mes = mes;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
+
+class ListaMeses{
+    constructor(){
+    this.raiz = null;
+    }
+
+    insertar(mes){
+        let nodo_nuevo = new Nodo(mes)
+        if (this.raiz == null){
+            this.raiz = nodo_nuevo;
+        }else{
+            let temporal = this.raiz;
+            while(temporal.siguiente != null){
+                temporal = temporal.siguiente;
+            }
+            temporal.siguiente = nodo_nuevo;
+            temporal.anterior = nodo_nuevo;
+        }
+    }
+    generarDot(){
+        let cadena="digraph arbol {\n" + 'graph [rankdir=LR]' + '\n' + 'node [shape = box, color=green , style=filled, fillcolor=lightskyblue];';
+        cadena+= this.generar_nodos(this.raiz)
+        cadena+="\n"
+        cadena+=this.enlazar(this.raiz)
+        cadena+="\n}"
+
+        console.log(cadena)
+    }
+
+    generar_nodos(raiz_actual){ 
+        let nodos =""
+        if(raiz_actual != null){
+            nodos+= "n"+raiz_actual.mes+"[label=\"" + "Id: "+raiz_actual.mes+"\"]\n"
+      
+        }
+        return nodos
+    }
+
+    enlazar(raiz_actual){
+        let cadena=""
+        if(raiz_actual != null){
+            cadena += this.enlazar(raiz_actual.siguiente)
+            if(raiz_actual.siguiente != null){
+                cadena+="n"+raiz_actual.mes + "-> n"+raiz_actual.siguiente.mes+"\n"
+            }
+            if(raiz_actual.anterior != null){
+                cadena+= "n"+raiz_actual.siguiente.mes+"-> n"+raiz_actual.mes +"\n"
+            }
+        
+
+            
+        }
+        return cadena
+    }
+
+}
+/*Meses = new ListaMeses()
+Meses.insertar(1)
+Meses.insertar(8)
+Meses.insertar(7)
+Meses.insertar(6)
+Meses.generarDot()*/
+export default ListaMeses;
